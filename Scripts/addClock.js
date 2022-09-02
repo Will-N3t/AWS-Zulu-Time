5e5f3b6a9e386dc728dffb9fbbab727ac7d85ba4
Will Chapman <will@n3twork.com>
2022-09-01 19:06:32 -0700

    Main clock
    Could still do with some work to improve the text size, and maybe double-check format is correct

---
+++ Scripts/addClock.js
@@ -0,0 +1,78 @@
+/*
+Will Chapman
+N3twork
+1st Sept 2022
+
+ */
+
+// Store the clock
+var clock = null;
+
+
+// Ensure leading zeroes
+function leadingZero(number)
+{
+    var text = number.toString();
+        
+    if (text.length < 2)
+    {
+        return "0"+text;
+    }
+    
+    return number;
+}
+
+
+// Set the clock text
+function clockTick()
+{
+    if (clock != null)
+    {
+        var date = new Date();
+        clock.innerHTML = date.getFullYear() + "/" + leadingZero(date.getMonth()) + "/" + leadingZero(date.getDate()) + "T"
+            + leadingZero(date.getHours()) + ":" + leadingZero(date.getMinutes()) + ":" + leadingZero(date.getSeconds()) + "Z";
+    }
+}
+
+
+// Add the clock
+function addClock()
+{
+    var menuContainer = document.getElementsByClassName('globalNav-033')[0];
+    var menu = menuContainer.children[2];
+    var textExample = menu.children[2].children[1].children[0].children[1].children[0].children[0].children[0];
+
+    // Set up outer element of clock
+    var clockContainer = document.createElement("div");
+    clockContainer.role = "menuitem";
+    clockContainer.classList = menu.children[0].classList;
+    
+    // Set up middle element of clock
+    var clockFrame = document.createElement("span");
+    clockContainer.role = "menuitem";
+    clockContainer.classList = textExample.classList;
+    
+    // Set up clock element
+    clock = document.createElement("button");
+    clock.classList = textExample.children[0].classList;
+    
+    var computedStyle = window.getComputedStyle(textExample.children[0]);
+    Array.from(computedStyle).forEach(key =>  clock.style.setProperty(key, computedStyle.getPropertyValue(key), computedStyle.getPropertyPriority(key)) );
+    clock.style.textAlign = "right";
+    clock.style.width = "100%";
+    clock.style.fontSize = (Number(clock.style.fontSize) * 1.25).toString();
+    clock.style.fontWeight = (Number(clock.style.fontWeight) * 1.5).toString();
+
+    clockFrame.appendChild(clock);
+    clockContainer.appendChild(clockFrame);
+    menu.insertBefore(clockContainer, menu.children[0]);
+
+    clockTick();
+}
+
+
+// Create the clock in the right place when the window is loaded
+window.onload = addClock;
+
+// Set the clock to tick every second
+setInterval(clockTick,1000);
